c225ccb2ce4e52672608255e9700862931658a61
Majid Joseph <majid.joseph7@gmail.com>
2020-05-03 23:31:56 -0400

    more cleaning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import './App.css';
 
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+
+import Dashboard from './Components/dashboard';
+import Login from './Components/login';
+import Register from './Components/register';
+
 function App() {
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
   return (
-    <Fragment></Fragment>
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route exact path="/login" render={props => !isAuthenticated ? (<Login {...props}/>) : (<Redirect to='/dashboard' />)}/>
+            <Route exact path="/register" render={props => <Register {...props}/>}/>
+            <Route exact path="/dashboard" render={props => <Dashboard {...props}/>}/>
+          </Switch>
+        </div>
+      </Router>
+    </Fragment>
   );
 }
 
---
+++ frontend/src/Components/register.js
@@ -0,0 +1,11 @@
+import React, { Fragment, useState } from 'react';
+
+const Register = () =>{
+    return(
+        <Fragment>
+            <h1>Register</h1>
+        </Fragment>
+    );
+}
+
+export default Register;
